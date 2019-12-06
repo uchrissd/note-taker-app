f9121fb39b41ae09e76c7d0a7dd55c95847dbf23
uchrissd <uchrissd@protonmail.com>
2019-12-06 14:42:03 -0800

    created function to read and write json file

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@
 
 var express = require("express");
 var path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
 var app = express();
@@ -9,15 +10,16 @@ var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(express.json());
 
 // Routes
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 // Read the db.json file and return all saved notes as JSON.
@@ -28,6 +30,21 @@ app.get("/api/notes", function(req, res) {
 //Post the stuff
 app.post("/api/notes", function(req, res) {
   var notes = req.body;
+  let noteText = { title: "Test Title", text: "Test text" };
+  noteText.title = notes.title;
+  noteText.text = notes.text;
+
+  fs.readFile("./db/db.json", "utf8", function noteCallBack(err, d) {
+    if (err) throw err;
+    console.log("this is the console log", d);
+    noteObject = JSON.parse(d);
+    noteObject.data.push(noteText);
+    noteJSON = JSON.stringify(noteObject);
+
+    fs.writeFile("./db/db.json", noteJSON, "utf8", err => {
+      if (err) throw err;
+    });
+  });
 });
 //Delete the stuff
 app.delete("/", function(req, res) {
